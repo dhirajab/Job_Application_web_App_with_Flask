970ea9b93310d2cce6752bfc2ec5878c4b0a2f72
Dhiraj <dhirajborkar002@gmail.com>
2024-01-20 10:55:58 +0530

    Showing Submission notification

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,  flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -31,6 +31,8 @@ def index():
                     email=email, date=date_obj, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+        flash(f"{first_name},Your form was submitted successfully!", "success")
+
 
     return render_template("index.html")
 
